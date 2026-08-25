7b42a573824a9b79ff0bc9d41cf34657b5304cc4
KenwarDhillon <kenwardhillon@gmail.com>
2026-08-24 22:45:16 -0700

    Added more code to rsa_keys.h-8/24/2026 10:7 PM-KD

--- rsa_keys.h
+++ rsa_keys.h
@@ -5,4 +5,29 @@
 
 using boost::multiprecision::cpp_int;
 
+struct RSAKeys {
+	cpp_int P;
+	cpp_int Q;
+	cpp_int N;
+	cpp_int T;
+	cpp_int E;
+	cpp_int D;
+};
+
+inline RSAKeys getRSAKeys() {
+	RSAKeys keys;
+
+	// These are my temporary RSA values for testing below;-KD
+	keys.P = 307;
+	keys.Q = 311;
+
+	keys.N = keys.P * keys.Q;
+	keys.T = (keys.P - 1) * (keys.Q -1);
+
+	keys.E = 65537;
+	keys.D = 54953;
+
+	return keys;
+}
+
 #endif
